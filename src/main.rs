34ed51e351c4398365c24a776989441636d9e66f
Chance <chanceusc@gmail.com>
2024-10-25 12:35:35 -0400

    :(

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
-use jsonptr::error::{ReportErr, ReportErrMut};
+use jsonptr::{error::Report, resolve, ReplaceError};
 
-fn main() {
+fn mutable() {
+    use jsonptr::error::ReportErrMut;
     let mut ptr = jsonptr::PointerBuf::parse("/example").unwrap();
-
-    let err = ptr.report_err().replace(2, "invalid").unwrap_err();
+    // 👍
+    let err: Report<ReplaceError> = ptr.report_err().replace(2, "invalid").unwrap_err();
 }
+
+fn immutable() {
+    use jsonptr::error::ReportErr;
+    let ptr = jsonptr::PointerBuf::parse("/example").unwrap();
+    // 👍
+    let err: Report<resolve::Error> = ptr
+        .report_err()
+        .resolve(&serde_json::Value::Null)
+        .unwrap_err();
+}
+
+fn both() {
+    use jsonptr::error::{ReportErr, ReportErrMut};
+
+    let mut ptr = jsonptr::PointerBuf::parse("/example").unwrap();
+    let err: Report<resolve::Error> = ptr
+        .report_err()
+        .resolve(&serde_json::Value::Null)
+        .unwrap_err();
+    // ❌ no method named `replace` found for struct `jsonptr::reporter::Imuutable` in the current scope
+    let err: Report<ReplaceError> = ptr.report_err().replace(2, "invalid").unwrap_err();
+}
+
+fn main() {}
